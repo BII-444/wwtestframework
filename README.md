ea084ce46c57066789129f1d54b8afa691ba19ef
Kane <strangebioman@gmail.com>
2021-12-06 23:17:22 -0500

    basic back-end

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # wwtestframework
-wwtestframework is a work-in-progress desktop application designed to fetch the header results from a google search. It runs using the latest version of the selenium chromedriver.
+wwtestframework is a work-in-progress desktop application designed to fetch the header results from a google search. It runs using the latest version of the selenium chromedriver. Go to https://chromedriver.chromium.org/downloads to download it, and place it in the same directory as the source files.
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -3,8 +3,9 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Collections.Generic;
 
-namespace wtestframework
+namespace ytestframework
 {
     public class Tests
     {
@@ -15,31 +16,34 @@ namespace wtestframework
         }
 
         [Test]
-        public void TestStringInput()
+        public void Test1()
         {
-            //USER INPUT
-
-
-            // SITE NAVIGATION
-
-            IWebDriver wdriver = new ChromeDriver();
-            // navigate to site
-            wdriver.Navigate().GoToUrl("https://www.google.com");
+            // allocate a browser drive
+            IWebDriver my_driver = new ChromeDriver();
+            // navigate to the Google homepage
+            my_driver.Navigate().GoToUrl("https://www.google.com");
             // find search bar
-            IWebElement searchbar = wdriver.FindElement(By.Name("q"));
+            IWebElement searchbar = my_driver.FindElement(By.Name("q"));
             // send input
-            searchbar.SendKeys("test");
-            //wait and submit???
-            wdriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(8);
+            searchbar.SendKeys("where is berlin");
+            //wait and submit
+            my_driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(8);
             searchbar.Submit();
-            // searchbar.SendKeys("\t");
 
+            // find h3 headers;
+            IList<IWebElement> all_results = my_driver.FindElements(By.CssSelector(".LC20lb.MBeuO.DKV0Md"));
+
+            my_driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            foreach(IWebElement each_result in all_results)
+            {
+                if(!String.IsNullOrEmpty(each_result.Text))
+                {
+                    Console.WriteLine("Result: " + each_result.Text);
+                }
+                // Console.WriteLine("Title: " + each_result.Text);
+                // System.out.println("Title : "+eachResult.getText()+", Link : "+eachResult.getAttribute("href"));
+            }
             Assert.Pass();
-        }
-        [OneTimeTearDown]
-        public void EndProcess()
-        {
-            // wdriver.Close();
         }
     }
 }
